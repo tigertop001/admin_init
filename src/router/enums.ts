bb6a76e526331b90f9c8db0cdf8ae5b65527f6b0
fannychu <tigertop001@gmail.com>
2024-11-08 01:07:28 +0800

    workflow: 8

--- src/router/enums.ts
+++ src/router/enums.ts
@@ -1,11 +1,6 @@
 // 完整版菜单比较多，将 rank 抽离出来，在此方便维护
 
 const home = 0, // 平台规定只有 home 路由的 rank 才能为 0 ，所以后端在返回 rank 的时候需要从非 0 开始
-  frame = 10,
-  permission = 12,
-  tenant = 13,
-  system = 14,
-  monitor = 15,
   menuoverflow = 25;
 
-export { home, frame, permission, tenant, system, monitor, menuoverflow };
+export { home, menuoverflow };
