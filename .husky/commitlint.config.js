d715f0d28292f2658feba553f6735e59007b836d
fannychu <tigertop001@gmail.com>
2024-11-08 19:00:29 +0800

    fix: kfjdkf

--- .husky/commitlint.config.js
+++ .husky/commitlint.config.js
@@ -5,7 +5,6 @@ export default {
   ignores: [commit => commit.includes("init")],
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-leading-blank": [2, "always"],
     "footer-leading-blank": [1, "always"],
     "header-max-length": [2, "always", 108],
     "subject-empty": [2, "never"],
