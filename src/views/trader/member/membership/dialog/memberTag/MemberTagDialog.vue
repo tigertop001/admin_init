880ce12254a3f7f89ac1d226002e562bec0aba57
fannychu <tigertop001@gmail.com>
2024-11-25 15:28:06 +0800

    fix: 1

--- src/views/trader/member/membership/dialog/memberTag/MemberTagDialog.vue
+++ src/views/trader/member/membership/dialog/memberTag/MemberTagDialog.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { PlusDialog } from "plus-pro-components";
+import { ElDescriptions, ElDescriptionsItem } from "element-plus";
 import { computed } from "vue";
 interface Props {
   visible: boolean;
@@ -13,24 +15,40 @@ const dialogVisible = computed({
 const emit = defineEmits<{
   "update:visible": [value: boolean];
 }>();
+const saveTag = () => {
+  console.log("保存标签");
+};
+
+// 定义按钮配置
+const footerButtons = {
+  confirm: {
+    text: "保存"
+  },
+  cancel: {
+    text: "取消"
+  }
+};
 </script>
 
 <template>
-  <el-dialog
+  <PlusDialog
     v-model="dialogVisible"
     title="会员标识详情"
     width="400px"
-    destroy-on-close
-    @update:model-value="val => emit('update:visible', val)"
+    :destroy-on-close="true"
+    :footer-buttons="footerButtons"
+    @confirm="saveTag"
   >
-    <div class="space-y-4">
+    <!-- <div class="space-y-4">
       <div class="flex items-center">
         <span class="w-24 text-gray-600">会员标识:</span>
         <span>{{ props.currentTag?.tag }}</span>
       </div>
-    </div>
-    <template #footer>
-      <el-button @click="emit('update:visible', false)">关闭</el-button>
-    </template>
-  </el-dialog>
+    </div> -->
+    <ElDescriptions :column="1" border>
+      <ElDescriptionsItem label="会员标识">
+        {{ props.currentTag?.tag }}
+      </ElDescriptionsItem>
+    </ElDescriptions>
+  </PlusDialog>
 </template>
